ba1f0ce8ed261afd2f840b70a16ad77d4195ed76
Lucas <lucas_emanuel_sc@hotmail.com>
2023-02-28 17:44:30 -0300

    fix: login Backend route and validations

--- back-end/src/middlewares/validateLogin.js
+++ back-end/src/middlewares/validateLogin.js
@@ -1,8 +1,11 @@
 const ErrorClass = require('../utils/ErrorClass');
 
 const validateLogin = (req, _res, next) => {
-    const { email, password } = req.body;
-    if (!email || !password) throw new ErrorClass(404, 'email or password required');
+  const { email, password } = req.body;
+  const validateEmail = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/gi;
+  if (!email || !password) throw new ErrorClass(404, 'email or password required');
+  if (password.length < 6) throw new ErrorClass(404, 'invalid password');
+  if (!validateEmail.test(email)) throw new ErrorClass(404, 'email invalid');
   next();
 };
 
--- back-end/src/routes/Login.routes.js
+++ back-end/src/routes/Login.routes.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
 const { loginController } = require('../controllers');
-// const validateLogin = require('../middlewares/validateLogin');
+const validateLogin = require('../middlewares/validateLogin');
 
 const router = Router();
 
 router.post(
   '/',
-  // validateLogin,
-  loginController, 
+  validateLogin,
+  loginController,
 );
 
 module.exports = router;

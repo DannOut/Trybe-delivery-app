ddae5d2fe8e59ef31a7a6fcaa92d778b1c1fc884
DannOut <danielxdn@gmail.com>
2023-03-01 19:11:34 -0300

    refact: testing components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,21 +4,21 @@ import Logo from './images/Logo.svg';
 import './App.css';
 
 import Login from './Pages/Login';
-import Redirect from './Pages/Redirect';
+import Redirect from './Pages/RedirectToLogin';
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/">
-        <div className="App">
-          <img src={ Logo } alt="Your SVG" />
-          <Redirect />
-        </div>
-      </Route>
       <Route exact path="/login">
         <div className="App">
           <img src={ Logo } alt="Your SVG" />
           <Login />
+        </div>
+      </Route>
+      <Route exact path="/">
+        <div className="App">
+          <img src={ Logo } alt="Your SVG" />
+          <Redirect />
         </div>
       </Route>
     </Switch>

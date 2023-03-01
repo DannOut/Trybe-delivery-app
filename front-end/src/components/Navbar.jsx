aa1a42ce847c6c5455d36f4d448926e8aa8ec82e
DannOut <danielxdn@gmail.com>
2023-03-01 20:21:22 -0300

    build(component creation): creating components to use in customer/products

---
+++ front-end/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function navbar() {
+  return (
+    <div>
+      <nav>
+        <div data-testid="customer_products__element-navbar-link-products">
+          link-products
+        </div>
+        <div data-testid="customer_products__element-navbar-link-orders">
+          Link Orders
+        </div>
+        <div data-testid="customer_products__element-navbar-user-full-name">
+          Link user-full-name
+        </div>
+        <div data-testid="customer_products__element-navbar-link-logout">
+          link-logout
+        </div>
+      </nav>
+    </div>
+  );
+}

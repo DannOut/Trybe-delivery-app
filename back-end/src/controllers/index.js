a6d7e3a3051b8211d0427f410bee8c05909692a0
Lucas <lucas_emanuel_sc@hotmail.com>
2023-03-01 15:42:31 -0300

    feat: Register Route Completed

--- back-end/src/controllers/index.js
+++ back-end/src/controllers/index.js
@@ -1,5 +1,7 @@
 const loginController = require('./Login.controller');
+const registerController = require('./Register.controller');
 
 module.exports = {
   loginController,
+  registerController,
 };
--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,5 +1,7 @@
 const loginRouter = require('./Login.routes');
+const registerRouter = require('./Register.routes');
 
 module.exports = {
   loginRouter,
+  registerRouter,
 };

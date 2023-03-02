c93aef8d0257834c221cb7d297ca74468a6733c3
DannOut <danielxdn@gmail.com>
2023-03-02 18:51:30 -0300

    fix(working on question 13): advancing in question 13

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-export default function navbar() {
+export default function Navbar() {
+  const { name } = JSON.parse(localStorage.getItem('user')) || '';
+  const history = useHistory();
+
+  const logout = () => {
+    localStorage.clear();
+    history.push('/login');
+  };
+
   return (
     <nav>
       <div data-testid="customer_products__element-navbar-link-products">
-        link-products
+        Products
       </div>
       <div data-testid="customer_products__element-navbar-link-orders">
-        Link Orders
+        Orders
       </div>
       <div data-testid="customer_products__element-navbar-user-full-name">
-        Link user-full-name
+        {name}
       </div>
       <button
         type="button"
         data-testid="customer_products__element-navbar-link-logout"
+        onClick={ logout }
       >
         link-logout
       </button>

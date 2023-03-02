fa4dfa7bf70741879268fd72e38c2158fa195282
DannOut <danielxdn@gmail.com>
2023-03-02 17:45:02 -0300

    feat(completed reqs 11, 12, 14): finishing req's 11, 12, 14

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,21 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const errorHandler = require('../middlewares/ErrorHandler');
-const { loginRouter, registerRouter, productsRouter, checkOutRouter } = require('../routes');
+const express = require("express");
+const cors = require("cors");
+const errorHandler = require("../middlewares/ErrorHandler");
+const {
+  loginRouter,
+  registerRouter,
+  productsRouter,
+  checkOutRouter,
+} = require("../routes");
 
 const app = express();
 app.use(cors());
-const teste = __dirname + "images";
-console.log('teste :>> ', teste);
 
 app.use(express.json());
-app.use("/images", express.static(teste));
+app.use("/images", express.static(`${__dirname}/../images`));
 
-app.get('/coffee', (_req, res) => res.status(418).end());
-app.use('/login', loginRouter);
-app.use('/register', registerRouter);
-app.use('/products', productsRouter);
-app.use('/checkout', checkOutRouter);
+app.get("/coffee", (_req, res) => res.status(418).end());
+app.use("/login", loginRouter);
+app.use("/register", registerRouter);
+app.use("/products", productsRouter);
+app.use("/checkout", checkOutRouter);
 app.use(errorHandler);
 
 module.exports = app;
--- front-end/src/components/ProductsCard.jsx
+++ front-end/src/components/ProductsCard.jsx
@@ -5,8 +5,9 @@ import { string, number } from 'prop-types';
 function ProductsCard({ id, name, urlImage, price }) {
   console.log('urlImage :>> ', urlImage);
   return (
-    <div>
+    <section data-testid="products">
       <p data-testid={ `customer_products__element-card-price-${id}` }>
+        R$
         {price}
       </p>
       <img
@@ -15,10 +16,32 @@ function ProductsCard({ id, name, urlImage, price }) {
         height="100px"
         data-testid={ `customer_products__img-card-bg-image-${id}` }
       />
-      <p data-testid={ `customer_products__element-card-title-${id}` }>
-        {name}
-      </p>
-    </div>
+      <p data-testid={ `customer_products__element-card-title-${id}` }>{name}</p>
+      <section>
+        <button
+          data-testid={ `customer_products__button-card-rm-item-${id}` }
+          type="button"
+          name="decreaseButton"
+          onClick={ (e) => onClick(e) }
+        >
+          -
+        </button>
+        <input
+          type="text"
+          data-testid={ `customer_products__input-card-quantity-${id}` }
+          name="quantity"
+          value={ 0 }
+        />
+        <button
+          data-testid={ `customer_products__button-card-add-item-${id}` }
+          type="button"
+          name="increaseButton"
+          onClick={ (e) => onClick(e) }
+        >
+          +
+        </button>
+      </section>
+    </section>
   );
 }
 

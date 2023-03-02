4e4bd16473bfedbf0dc52eca603e63f1baf75954
Lucas <lucas_emanuel_sc@hotmail.com>
2023-03-02 19:46:57 -0300

    feat: sales route not ready!

---
+++ back-end/src/services/Sales.service.js
@@ -0,0 +1,44 @@
+const { Sale, Product, SaleProduct } = require('../database/models');
+const ErrorClass = require('../utils/ErrorClass');
+const { decodeToken } = require('../auth/jwtFunctions');
+
+// const validatePersons = async (customerEmail, sellerEmail) => {
+//   const customer = await User
+//   .findOne({ where: { email: customerEmail, role: 'customer' }, raw: true });
+//   const seller = await User
+//   .findOne({ where: { email: sellerEmail, role: 'seller' }, raw: true });
+//   if (!customer) throw new ErrorClass(404, 'customer not Found');
+//   if (!seller) throw new ErrorClass(404, 'seller not Found');
+//   return { userId: customer.id, sellerId: seller.id };
+// };
+
+const getAllSales = async (id, token) => {
+  const { role } = decodeToken(token);
+  const roleFound = role === 'customer' ? 'userId' : 'sellerId';
+  const sales = await Sale.findAll({ where: { [roleFound]: id }, raw: true });
+  if (!sales) throw new ErrorClass(404, 'Sales not found!');
+  return sales;
+};
+
+const getSalesById = async (id, token) => {
+  const { role } = decodeToken(token);
+  if (role === 'customer') throw new ErrorClass(401, 'Acess unauthorized');
+  const checkSale = await Sale.findByPk(id);
+  if (!checkSale) throw new ErrorClass(404, 'Sale not found!');
+  const products = await SaleProduct.findAll({
+    where: { saleId: id },
+    attributes: ['quantity'],
+    include: [
+      {
+        model: Product,
+        as: 'product',
+        attributes: ['id', 'name', 'price', 'urlImage'],
+      }],
+  });
+  return { products, totalPrice: checkSale.totalPrice };
+};
+
+module.exports = {
+  getAllSales,
+  getSalesById,
+};

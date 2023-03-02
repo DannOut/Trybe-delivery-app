902494d931eb1e6324226591593d75de26a2859b
DannOut <danielxdn@gmail.com>
2023-03-02 08:35:54 -0300

    feat(creating provider): starting to create provider

---
+++ front-end/src/context/Provider.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Context from './Context';
+
+function Provider({ children }) {
+  return <Context.Provider>{children}</Context.Provider>;
+}
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Provider;

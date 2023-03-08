ccb40057785e2e5ddf89ca9086e422d37f003a44
DannOut <danielxdn@gmail.com>
2023-03-08 09:21:06 -0300

    bugfix: finish repairs in admin page

--- back-end/src/services/Admin.service.js
+++ back-end/src/services/Admin.service.js
@@ -6,7 +6,7 @@ const ErrorClass = require('../utils/ErrorClass');
 const registerUser = async (newUser) => {
   const { role, email, name } = newUser;
   const user = await User.findOne({ where: { email }, raw: true });
-  if (user) throw new ErrorClass(404, 'Email is already being used');
+  if (user) throw new ErrorClass(409, 'Email is already being used');
   const hashPassword = decodePassword(newUser.password);
   const userCreated = await User.create({ password: hashPassword, role, email, name });
   const { password, ...userWithoutPassword } = userCreated.dataValues;

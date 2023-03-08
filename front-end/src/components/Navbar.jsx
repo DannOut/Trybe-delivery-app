a9a1532a4e15ae132b3cddd21bdf98c7599583a4
DannOut <danielxdn@gmail.com>
2023-03-08 14:33:30 -0300

    fix(refacting classnames to work in new config): refact classnames

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useHistory, NavLink } from 'react-router-dom';
-// import styles from './Navbar.module.css';
-// import Logo from '../images/Logo.svg';
+import styles from './Navbar.module.css';
+import Logo from '../images/Logo.svg';
 
 export default function Navbar() {
   const { name, role } = JSON.parse(localStorage.getItem('user')) || '';
@@ -13,61 +13,36 @@ export default function Navbar() {
   };
 
   return (
-  // <<<<<<< HEAD
-    <nav>
-      <NavLink to="/customer/products">
-        <div data-testid="customer_products__element-navbar-link-products">
-          Products
-        </div>
-      </NavLink>
-      <NavLink to={ `/${role}/orders` }>
-        <div data-testid="customer_products__element-navbar-link-orders">
-          Orders
-        </div>
-      </NavLink>
-      <div data-testid="customer_products__element-navbar-user-full-name">
-        {name}
+    <header className={ styles.menu }>
+      <div>
+        <NavLink to="/customer/products" className={ styles.products }>
+          <div
+            data-testid="customer_products__element-navbar-link-products"
+          >
+            Products
+          </div>
+        </NavLink>
+        <NavLink to={ `/${role}/orders` } className={ styles.orders }>
+          <div
+            data-testid="customer_products__element-navbar-link-orders"
+          >
+            Orders
+          </div>
+        </NavLink>
+        <img src={ Logo } alt="" />
       </div>
-      <button
-        type="button"
-        data-testid="customer_products__element-navbar-link-logout"
-        onClick={ logout }
-      >
-        link-logout
-      </button>
-    </nav>
-  // =======
-  //     <header className={ styles.menu }>
-  //       <div>
-  //         <a
-  //           href="#sadas"
-  //           className={ styles.products }
-  //           data-testid="customer_products__element-navbar-link-products"
-  //         >
-  //           Produtos
-  //         </a>
-  //         <a
-  //           href="#asd"
-  //           className={ styles.orders }
-  //           data-testid="customer_products__element-navbar-link-orders"
-  //         >
-  //           Meu Pedidos
-  //         </a>
-  //       </div>
-  //       <img src={ Logo } alt="" />
-  //       <nav>
-  //         <div data-testid="customer_products__element-navbar-user-full-name">
-  //           {name}
-  //         </div>
-  //         <button
-  //           type="button"
-  //           data-testid="customer_products__element-navbar-link-logout"
-  //           onClick={ logout }
-  //         >
-  //           Sair
-  //         </button>
-  //       </nav>
-  //     </header>
-  // >>>>>>> main-group-21-dev
+      <nav>
+        <div data-testid="customer_products__element-navbar-user-full-name">
+          {name}
+        </div>
+        <button
+          type="button"
+          data-testid="customer_products__element-navbar-link-logout"
+          onClick={ logout }
+        >
+          Sair
+        </button>
+      </nav>
+    </header>
   );
 }
